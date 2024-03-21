e0273f0b9dd17893a69bb149d9e28d2a3d740b19
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-03-20 20:38:18 -0600

    got the routes working

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,13 +1,35 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [director, setDirectors] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+      .then(r => r.json())
+      .then(data => {
+        setDirectors(data)
+      })
+  }, [])
+
+  const directorInfo = director.map((director) => {
+    const directorMovie = director.movies.map((movie, index) => { return <li key={index}>{movie}</li> })
+    return <article key={director.id}>
+      <h2>{director.name}</h2>
+      <ul>
+        {directorMovie}
+      </ul>
+    </article>
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directorInfo}
       </main>
     </>
   );
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,31 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [currentMovie, setCurrentMovie] = useState({})
+  const movieParam = useParams()
+  const movieId = movieParam.id
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${movieId}`)
+      .then(r => r.json())
+      .then((data) => {
+        setCurrentMovie(data)
+      })
+  }, [movieId])
+
+  const movieGenre = currentMovie.genres?.map(genre => <span key={genre}>{genre}</span>)
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{currentMovie.title}</h1>
+        <p>{currentMovie.time} minutes</p>
+        {movieGenre}
       </main>
     </>
   );
